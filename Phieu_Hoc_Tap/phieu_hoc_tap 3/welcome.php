6427e067d0ce50f589197c9ea5f5dba630684d29
CaoDucDao <caoducdao17022005@gmail.com>
2025-11-23 01:11:31 +0700

    phieu 3

---
+++ Phieu_Hoc_Tap/phieu_hoc_tap 3/welcome.php
@@ -0,0 +1,21 @@
+<?php
+
+session_start();
+
+
+if (isset($_SESSION['username'])) {
+
+
+    $loggedInUser = $_SESSION['username'];
+
+    echo "<h1>Chào mừng trở lại, $loggedInUser!</h1>";
+    echo "<p>Bạn đã đăng nhập thành công.</p>";
+
+    echo '<a href="login.html">Đăng xuất </a>';
+
+} else {
+
+    header('Location: login.html');
+    exit();
+}
+?>
